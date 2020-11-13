d42db826d1813e35284da2e6e59b1673316786b5
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-11-13 23:03:04 +0100

    Everything is working as good as it should be Or as good as we can imagine

--- src/api/hello/sayBey/sayBey.resolvers.ts
+++
@@ -1,7 +0,0 @@
-const resolvers = {
-    Query: {
-        sayBey: () => 'Hey Hello How are you'
-    }
-};
-
-export default resolvers;
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,69 +1,110 @@
-import { IsEmail } from 'class-validator'
+import bcrypt from 'bcrypt';
+import { IsEmail } from 'class-validator';
 import { 
-    BaseEntity,
-    Column,
-    CreateDateColumn,
-    Entity,
-    PrimaryGeneratedColumn,
-    UpdateDateColumn
+	BaseEntity,
+	BeforeInsert,
+	BeforeUpdate,
+	Column,
+	CreateDateColumn,
+	Entity,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn
 } from 'typeorm';
+
+/**
+ * The Number of times our password should be hashed
+ */
+const BCRYPT_ROUNDS = 10;
 
 @Entity()
 class User extends BaseEntity {
-    @PrimaryGeneratedColumn() id: number;
+	@PrimaryGeneratedColumn() id: number;
 
-    @Column({ type: 'text', unique: true })
-    @IsEmail()
-    email: string;
+	@Column({ type: 'text', unique: true })
+	@IsEmail()
+	email: string;
 
-    @Column({ type: 'boolean', default: false })
-    verifiedEmail: boolean;
+	@Column({ type: 'boolean', default: false })
+	verifiedEmail: boolean;
 
-    @Column({ type: 'text' })
-    firstName: string;
+	@Column({ type: 'text' })
+	firstName: string;
 
-    @Column({ type: 'text' })
-    lastName: string;
+	@Column({ type: 'text' })
+	lastName: string;
 
-    @Column({ type: 'int'})
-    age: number;
+	@Column({ type: 'int'})
+	age: number;
 
-    @Column({ type: 'text'})
-    password: string;
+	@Column({ type: 'text'})
+	password: string;
 
-    @Column({ type: 'text'})
-    phoneNumber: string;
+	@Column({ type: 'text'})
+	phoneNumber: string;
 
-    @Column({ type: 'boolean', default: false })
-    verifiedPhoneNumber: boolean;
+	@Column({ type: 'boolean', default: false })
+	verifiedPhoneNumber: boolean;
 
-    @Column({ type: 'text'})
-    profilePhoto: string;
+	@Column({ type: 'text'})
+	profilePhoto: string;
 
-    @Column({ type: 'boolean', default: false})
-    isDriving: boolean;
+	@Column({ type: 'boolean', default: false})
+	isDriving: boolean;
 
-    @Column({ type: 'boolean', default: false})
-    isRiding: boolean;
+	@Column({ type: 'boolean', default: false})
+	isRiding: boolean;
 
-    @Column({ type: 'boolean', default: false})
-    isTaken: boolean;
+	@Column({ type: 'boolean', default: false})
+	isTaken: boolean;
 
-    @Column({ type: 'double precision', default: 0 })
-    lastLng: number;
+	@Column({ type: 'double precision', default: 0 })
+	lastLng: number;
 
-    @Column({ type: 'double precision', default: 0 })
-    lastLat: number;
+	@Column({ type: 'double precision', default: 0 })
+	lastLat: number;
 
-    @Column({ type: 'double precision', default: 0 })
-    lastOrientation: number;
+	@Column({ type: 'double precision', default: 0 })
+	lastOrientation: number;
 
-    get fullName(): string {
-        return `${this.firstName} ${this.lastName}`;
-    }
+	@CreateDateColumn() createdAt: string;
+	@UpdateDateColumn() updatedAd: string;
 
-    @CreateDateColumn() createdAt: string;
-    @UpdateDateColumn() updatedAd: string;
+	get fullName(): string {
+		return `${this.firstName} ${this.lastName}`;
+	}
+
+	@BeforeInsert()
+	@BeforeUpdate()
+
+	/**
+	 * @desc Asynchronus funciton to hash Our password
+	 * @param { Password } The_Pass_to_be_hashed
+	 * @returns { Promise<void> } The Hashed Password
+	 */
+	async savePassword(): Promise<void> {
+		if (this.password) {
+			const hashedPassword = await this.hashPassword(this.password);
+			this.password = hashedPassword;
+		}
+	}
+
+	/**
+	 * @desc The function to check if both passes are identic 
+	 * @param { password }
+	 * @return { Promise<void> }
+	 */
+	public comparePassword(password: string = ''): Promise<boolean> {
+		return bcrypt.compare(password, this.password);
+	}
+
+	/**
+	 * @desc The Private function to hash the pass
+	 * @param { password }
+	 * @return { Promise<string> } The hashed pass
+	 */
+	private hashPassword(password: string): Promise<string> {
+		return bcrypt.hash(password, BCRYPT_ROUNDS);
+	}
 }
 
 export default User;
